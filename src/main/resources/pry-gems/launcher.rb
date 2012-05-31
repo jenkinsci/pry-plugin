82d14e6d92fe9f432ec169cedf8b301d0e8ab477
Kohsuke Kawaguchi <kk@kohsuke.org>
2012-05-31 09:20:37 -0700

    Clean up the variable binding and avoid signal handling.
    JRuby fails to restore JVM default signal handler, so if we trap SIGINT
    we won't be able to Ctrl+C

--- src/main/resources/pry-gems/launcher.rb
+++ src/main/resources/pry-gems/launcher.rb
@@ -4,7 +4,12 @@ require 'pry'
 class Launcher
     # bootstraps Pry.
     def self.start(root,i,o,cmd)
-        Pry.config.pager=false;
+        Pry.config.pager=false
+
+        # JRuby has a bug in signal handling such that trying to intercept SIGINT will make it impossible
+        # for JVM to exit with Ctrl+C
+        Pry.config.should_trap_interrupts=false
+
         o.sync = true;      # otherwise you don't see output until it spills from the buffer
 
         i.singleton_class.class_eval do
@@ -28,6 +33,11 @@ class Launcher
         i.out = o
         i.cmd = cmd
 
-        Pry.start(root, :input => i, :output => o)
+        Pry.start(create_binding(root,i,o), :input => i, :output => o, :quiet => true)
+    end
+
+    # create binding to be used in pry
+    def self.create_binding(jenkins,input,output)
+        binding
     end
 end
